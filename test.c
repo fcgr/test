2bc5b31ac0474391d4b2834597a61e40943e1057
Felipe Guerra <fcgr@cin.ufpe.br>
2017-08-09 09:30:14 -0300

    c file included

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void hmm (void);
+void hmmm (void);
+
+int main (int argc, char **argv) {
+    hmm();
+    hmmm();
+    return 0;
+}
+
+void hmm (void) {
+    printf("hmm");
+}
+
+void hmmm (void) {
+    printf("hmmm");
+}
